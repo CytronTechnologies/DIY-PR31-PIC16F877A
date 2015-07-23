82c4eed98ef9c2e3aaa41f26bcf6415e651ba201
CytronTrainee <cytontrainee@gmail.com>
2015-07-23 12:32:45 +0800

    DIY PR31
    Updated version.

---
+++ MPLABX/pr31.X/build/default/production/_ext/1142487903/PR31_final.p1.d
@@ -0,0 +1,2 @@
+build/default/production/_ext/1142487903/PR31_final.p1:	\
+	../../Source\ file/i2c_ar1000.h	\
---
+++ MPLABX/pr31.X/build/default/production/_ext/1142487903/i2c_ar1000.p1.d
@@ -0,0 +1,2 @@
+build/default/production/_ext/1142487903/i2c_ar1000.p1:	\
+	../../Source\ file/i2c_ar1000.h	\
